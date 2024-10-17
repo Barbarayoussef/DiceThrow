1321b57794287ec089169cde3ea8bf0feb205399
Barbarayoussef <barayoussef12@yahoo.com>
2024-10-17 11:37:27 -0400

    Implement a button in the main activity to find the fragment and change it.

--- app/src/main/java/edu/temple/dicethrow/MainActivity.kt
+++ app/src/main/java/edu/temple/dicethrow/MainActivity.kt
@@ -1,14 +1,20 @@
 package edu.temple.dicethrow
 
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
+import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        findViewById<Button>(R.id.rollDiceButton).setOnClickListener{
+            ( supportFragmentManager
+                .findFragmentById(R.id.diceContainer) as DieFragment ).throwDie() //it return an object of type fragment(refernce type) to tell the compiler to refernce the diefragment we casting
+
+
+        }
+
     }
 }
