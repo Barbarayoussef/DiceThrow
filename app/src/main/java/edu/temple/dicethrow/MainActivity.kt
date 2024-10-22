ec738f21225f5c4dc4dc3f581110ab74688e1b19
Barbarayoussef <barayoussef12@yahoo.com>
2024-10-22 13:51:47 -0400

    Making another fragment to display a button

--- app/src/main/java/edu/temple/dicethrow/MainActivity.kt
+++ app/src/main/java/edu/temple/dicethrow/MainActivity.kt
@@ -4,7 +4,7 @@ import android.os.Bundle
 import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), ButtonFragment.ButtonInterface {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -21,10 +21,15 @@ class MainActivity : AppCompatActivity() {
         }
 
 
-        findViewById<Button>(R.id.rollDiceButton).setOnClickListener{
+       /* findViewById<Button>(R.id.rollDiceButton).setOnClickListener{
             ( supportFragmentManager
                 .findFragmentById(R.id.diceContainer) as DieFragment ).throwDie() //it return an object of type fragment(refernce type) to tell the compiler to refernce the diefragment we casting
-        }
+        }*/
 
+    }
+
+    override fun buttonClicked() {
+        ( supportFragmentManager
+            .findFragmentById(R.id.diceContainer) as DieFragment ).throwDie()
     }
 }
